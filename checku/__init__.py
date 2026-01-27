40c2977ab381689da9af232b9a80a97af02eeb19
Juan C. Villada <jvillada@lbl.gov>
2026-01-27 17:25:38 +0000

    Bump version to 0.1.1 and update SHA256 checksums in recipe.yaml and pixi.toml

--- checku/__init__.py
+++ checku/__init__.py
@@ -2,7 +2,7 @@
 CheckU: UNI56 marker completeness profiling for microbial genomes.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Juan C. Villada"
 __email__ = "jvillada@lbl.gov"
 __title__ = "checku"
